81d58a27dfd0b5af25489d094d06006ec433ae38
EdgeLordKirito <113261869+EdgeLordKirito@users.noreply.github.com>
2024-06-29 00:50:54 +0200

    Added SalvationsEdge Raid specific Bingo values

--- Raids/SalvationsEdge/Edge.js
+++ Raids/SalvationsEdge/Edge.js
@@ -1,4 +1,10 @@
 // Define your list of specific items
 const raidSpecificValues = [
 	//fill with salvations edge stuff
+	"Fails Jump Test",
+	"Dies to Buff times four",
+	"Does not Shoot Blight in Second Encounter",
+	"Picks up Wrong Symbol in Verity",
+	"Screws Up Dissection in Verity",
+	"Dies with Glyphbreaker",
 ];
